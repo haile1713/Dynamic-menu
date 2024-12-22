2bf501b3195499e510c6ffa039bb3aeaa9590271
haile <haileleulfiseha@gmail.com>
2024-12-22 10:47:38 +0300

    images added

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const MenuItem = ({ name, img, price }) => {
+const MenuItem = ({ name, img, price, desc }) => {
   return (
     <div className="menu-item">
-      <img src={img} alt={name} className="menu-item-image" />
+      <div className="menu-item-image-wrapper">
+        <img src={img} alt={name} className="menu-item-image" />
+        <div className="menu-item-overlay">
+          <p className="menu-item-description">{desc}</p>
+        </div>
+      </div>
       <div className="menu-item-details">
         <h3>{name}</h3>
         <p className="menu-item-price">{price}</p>
--- src/data/menuData.js
+++ src/data/menuData.js
@@ -1,58 +1,70 @@
+import specialSaladImg from "../assets/images/special salad.jpg";
+import normalSaladImg from "../assets/images/normal salad.jpg";
+import eggtoastImg from "../assets/images/egg toast.jpg";
+import BaguetteImg from "../assets/images/Baguette.jpg";
+import teaImg from "../assets/images/tea.jpg";
+import redteaImg from "../assets/images/red tea.jpg";
+import EspressoImg from "../assets/images/esspreso.jpg";
+import coffeeImg from "../assets/images/coffee.jpg";
+import cappucinoImg from "../assets/images/cappucino.jpg";
+
 const menuData = [
-  {
-    name: "Special Salad",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKI5ZrBVvqIDC3eV_lXdN2KX4QC9Cv2KqIQ&s",
-    price: "700 Birr",
-    category: "Salads 🥗",
-  },
-  {
-    name: "Basic Salad",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKI5ZrBVvqIDC3eV_lXdN2KX4QC9Cv2KqIQ&s",
-    price: "900 Birr",
-    category: "Salads 🥗",
-  },
-  {
-    name: "Egg on toast",
-    img: "https://drive.google.com/uc?id=1v8fhiCGfZOLNzhbfOW3WSZu6sjZWwxuM", // Converted to direct download
-    price: "700 Birr",
-    category: "Brunch 🥖",
-  },
-  {
-    name: "Baguette",
-    img: "https://drive.google.com/uc?id=1JYlmUKvhNMUlKqAr7d8HWJKnl94M9-8u", // Converted to direct download
-    price: "900 Birr",
-    category: "Brunch 🥖",
-  },
-  {
-    name: "Tea",
-    img: "https://drive.google.com/uc?id=1tv_Y0RngrhRitbqEIFaqbJjJkXGPNW8H", // Converted to direct download
-    price: "300 Birr",
-    category: "Drinks ☕️",
-  },
-  {
-    name: "Red tea",
-    img: "https://drive.google.com/uc?id=1iQO2XdIKNBJdc5KgprFS8BPdUbOcPED2", // Converted to direct download
-    price: "400 Birr",
-    category: "Drinks ☕️",
-  },
-  {
-    name: "Espresso",
-    img: "https://drive.google.com/uc?id=1uyoN9j550TW2Pl4ll1TAHvkK5EvrHwn7", // Converted to direct download
-    price: "5 Birr",
-    category: "Drinks ☕️",
-  },
-  {
-    name: "Coffee",
-    img: "https://drive.google.com/uc?id=1avBZ-ECrB5CrokB8rqn_d7v6TkY42bbh", // Converted to direct download
-    price: "3 Birr",
-    category: "Drinks ☕️",
-  },
-  {
-    name: "Cappuccino",
-    img: "https://drive.google.com/uc?id=1avBZ-ECrB5CrokB8rqn_d7v6TkY42bbh", // Converted to direct download
-    price: "4 Birr",
-    category: "Drinks ☕️",
-  },
+	{
+		name: "Special Salad",
+		img: specialSaladImg, // Use the imported image
+		price: "900 Birr",
+		category: "Salads 🥗",
+		desc: "A delicious mix of fresh greens and house dressing.",
+	},
+	{
+		name: "Normal Salad",
+		img: normalSaladImg, // Use the imported image
+		price: "700 Birr",
+		category: "Salads 🥗",
+		desc: "A healthy combination of vegetables and spices.",
+	},
+	{
+		name: "Egg on toast",
+		img: eggtoastImg, // Converted to direct download
+		price: "700 Birr",
+		category: "Brunch 🥖",
+	},
+	{
+		name: "Baguette",
+		img: BaguetteImg, // Converted to direct download
+		price: "900 Birr",
+		category: "Brunch 🥖",
+	},
+	{
+		name: "Tea",
+		img: teaImg, // Converted to direct download
+		price: "300 Birr",
+		category: "Drinks ☕️",
+	},
+	{
+		name: "Red tea",
+		img: redteaImg, // Converted to direct download
+		price: "400 Birr",
+		category: "Drinks ☕️",
+	},
+	{
+		name: "Espresso",
+		img: EspressoImg, // Converted to direct download
+		price: "5 Birr",
+		category: "Drinks ☕️",
+	},
+	{
+		name: "Coffee",
+		img: coffeeImg, // Converted to direct download
+		price: "3 Birr",
+		category: "Drinks ☕️",
+	},
+	{
+		name: "Cappuccino",
+		img: cappucinoImg, // Converted to direct download
+		price: "4 Birr",
+		category: "Drinks ☕️",
+	},
 ];
 
 export default menuData;
